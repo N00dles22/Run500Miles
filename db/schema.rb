d6712908ffda6766f822d7d0e37dc3fdb83a9cfc
Nick Gallegos <nicholas.gallegos@gmail.com>
2011-11-30 13:56:56 -0700

    aed authentication logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111130192631) do
+ActiveRecord::Schema.define(:version => 20111130203928) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(:version => 20111130192631) do
     t.datetime "updated_at"
     t.string   "fname"
     t.string   "lname"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
