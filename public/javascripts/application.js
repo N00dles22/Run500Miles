e22ef619ab4f265097b11f2beae63904cc23d260
Nick Gallegos <nicholas.gallegos@gmail.com>
2011-12-06 13:22:42 -0700

    wrapped long log comments

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+//<SCRIPT LANGUAGE="JavaScript">
+//<!-- Dynamic Version by: Nannette Thacker -->
+//<!-- http://www.shiningstar.net -->
+//<!-- Original by :  Ronnie T. Moore -->
+//<!-- Web Site:  The JavaScript Source -->
+//<!-- Use one function for multiple text areas on a page -->
+//<!-- Limit the number of characters per textarea -->
+//<!-- Begin
+function textCounter(field,cntfield,maxlimit) {
+if (field.value.length > maxlimit) // if too long...trim it!
+    field.value = field.value.substring(0, maxlimit);
+// otherwise, update 'characters left' counter
+else
+    cntfield.value = maxlimit - field.value.length;
+}
+//  End -->
+//</script>
