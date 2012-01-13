667f2c1ab637721057ef66bc3b5fe858bc022c14
Nick Gallegos <nicholas.gallegos@gmail.com>
2012-01-13 16:49:37 -0700

    application.js commit after merge messed it up

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 
-// This is used to toggle div visibilities
-function setDivVisibility(showItem, hideItems) {
-  document.getElementById(showItem).style.display = 'block';
-  for (var i = 0; i < hideItems.length; i++) {
-    document.getElementById(hideItems[i]).style.display = 'none';
-  }
-}
-//<SCRIPT LANGUAGE="JavaScript">
-//<!-- Dynamic Version by: Nannette Thacker -->
-//<!-- http://www.shiningstar.net -->
-//<!-- Original by :  Ronnie T. Moore -->
-//<!-- Web Site:  The JavaScript Source -->
-//<!-- Use one function for multiple text areas on a page -->
-//<!-- Limit the number of characters per textarea -->
-//<!-- Begin
-function textCounter(field,cntfield,maxlimit) {
-if (field.value.length > maxlimit) // if too long...trim it!
-    field.value = field.value.substring(0, maxlimit);
-// otherwise, update 'characters left' counter
-else
-    cntfield.value = maxlimit - field.value.length;
-}
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-28163203-1']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-//  End -->
-//</script>
-=======
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
@@ -73,4 +35,3 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
->>>>>>> graphs
